9aa0647658489bdc724128edfee7c2e49ef94bf8
migellal <michalgellert@gmail.com>
2017-04-24 05:50:16 +0200

    paths

--- dayview/src/main/java/pl/digitalzombielab/dayview/DayView.kt
+++ dayview/src/main/java/pl/digitalzombielab/dayview/DayView.kt
@@ -1,10 +1,7 @@
 package pl.digitalzombielab.dayview
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
@@ -28,6 +25,7 @@ class DayView : View {
     private var width = 0.0F
     private var startLeft = 0.0F
     private var startTop = 0.0F
+    private var startBar = 0.0F
 
     var barColor = -12627531
         set(value) {
@@ -96,16 +94,16 @@ class DayView : View {
         textMonthPaint.color = textColor
         backgroundPaint.style = Paint.Style.FILL
         borderPaint.style = Paint.Style.STROKE
-        borderPaint.strokeWidth = 10F
+        borderPaint.strokeWidth = 1F
         barPaint.style = Paint.Style.FILL
 
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawRect(startLeft, startTop, width, height, backgroundPaint)
-        canvas?.drawRect(startLeft, startTop, width, height, borderPaint)
-        canvas?.drawRect(80F, 80F, 350F, 350F, barPaint)
+        canvas?.drawPath(makeBackgroundPath(), backgroundPaint)
+        canvas?.drawPath(makeBorderPath(), borderPaint)
+        //canvas?.drawPath(makeBarPath(), barPaint)
     }
 
     override fun onSizeChanged(xNew: Int, yNew: Int, xOld: Int, yOld: Int) {
@@ -114,14 +112,38 @@ class DayView : View {
         height = yNew.toFloat()
         startLeft = paddingLeft.toFloat()
         startTop = paddingTop.toFloat()
-        val xpad = (paddingLeft + paddingRight).toFloat()
-        val ypad = (paddingTop + paddingBottom).toFloat()
-        width -= xpad
-        height -= ypad
+        width -= paddingRight
+        height -= paddingTop
+        startBar = height / 6
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    private fun makeBarPath(): Path {
+        val path = Path()
+        path.moveTo(startLeft, startBar)
+        path.lineTo(startLeft, startTop)
+        path.lineTo(width + 1, startTop)
+        path.lineTo(width + 1, startBar)
+        path.close()
+        return path
+    }
+
+    private fun makeBorderPath(): Path {
+        val path = Path()
+        path.moveTo(startLeft, startBar)
+        path.lineTo(startLeft, height)
+        path.lineTo(width, height)
+        path.lineTo(width, startBar)
+        return path
+    }
+
+    private fun makeBackgroundPath(): Path {
+        val path = makeBorderPath()
+        path.close()
+        return path
     }
 
     private fun drawTextCenter(canvas: Canvas?, paint: Paint, text: String) {
